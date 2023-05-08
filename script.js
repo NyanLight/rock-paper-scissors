9d814a8c38b3a4eab1ed214343693c12e0d80e8e
NyanLight <128150876+NyanLight@users.noreply.github.com>
2023-05-09 02:57:01 +0400

    basic playeble prorotype

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let items = ["rock", "paper", "scissors"];
 function getComputerChoice() {
     return items[Math.floor(Math.random()*items.length)];
 }
+
 function oneRound(playerSelection, computerSelection) {
     playerSelection = prompt("Choose your weapon!").toLowerCase();
     computerSelection = getComputerChoice();
@@ -13,7 +14,7 @@ function oneRound(playerSelection, computerSelection) {
         return "You lose! Rock beats scissors";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         return "You lose! Paper beats rock";
-    } else if (playerSelectionq === "rock" && computerSelection === "scissors") {
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You win! Rock beats scissors";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         return "You win! Paper beats rock";
@@ -21,3 +22,22 @@ function oneRound(playerSelection, computerSelection) {
         return "You lose! Scissors beat paper";
     } 
 }
+
+function isItVictory() {
+check = oneRound().slice(0, 5);
+if (check == "You w") {
+    return 1;
+} else {
+    return 0;
+}
+}
+
+function game(){
+    score = isItVictory() + isItVictory() + isItVictory () + isItVictory() + isItVictory ();
+    if (score < 3) {
+        console.log("You lose");
+    } else {
+        console.log("You win!");
+    }
+}
+
