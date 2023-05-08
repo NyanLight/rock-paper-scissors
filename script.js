6abaef7a6f4b8e1c70e2980ce0809b8e742e2555
NyanLight <128150876+NyanLight@users.noreply.github.com>
2023-05-09 01:35:27 +0400

    add oneRound function

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ let items = ["rock", "paper", "scissors"];
 function getComputerChoice() {
     return items[Math.floor(Math.random()*items.length)];
 }
+function oneRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Choose your weapon!").toLowerCase();
+    computerSelection = getComputerChoice();
+    if (playerSelection === computerSelection) {
+        return "Oh, that's draw!"
+    } else if (playerSelection === "scissors"  && computerSelection === "paper") {
+        return "You win! Scissors beat paper";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats rock";
+    } else if (playerSelectionq === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats rock";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beat paper";
+    } 
+}
