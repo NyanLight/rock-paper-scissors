d435f754a9dd8304fbca4c48f236cd635f388fd3
NyanLight <128150876+NyanLight@users.noreply.github.com>
2023-08-27 19:30:02 +0400

    add real-time score display and winner anouncement

--- script.js
+++ script.js
@@ -6,30 +6,37 @@ function getComputerChoice() { // a function get a random item from the array ab
 function getPlayerChoice() { // create a function to get player's input and make it case insensetive using toLowerCase method
     return prompt('Choose your fighter!').toLowerCase(); 
 }
-let playerScore;  //  create a variable to store player's score
-let computerScore; // create a variable to store player's score
+let playerScore = 0;  //  create a variable to store player's score
+let computerScore = 0; // create a variable to store player's score
 
 function oneRound (playerChoice = getPlayerChoice(), computerChoice = getComputerChoice()) { //function to play one round of RPS game using conditional statement to check who wins
     if (computerChoice === playerChoice) {
         display.textContent = ("It's tie round");
+        checkWinner();
     } else if (computerChoice === "paper" && playerChoice === "rock") {
         display.textContent = ("Paper beats a rock, you lose the round!");
         ++computerScore;
+        checkWinner();
     } else if (computerChoice === "rock" && playerChoice === "paper") {
         display.textContent = ("Paper beats a rock, you win the round!");
         ++playerScore;
+        checkWinner();
     } else if (computerChoice === 'paper' && playerChoice === 'scissors') {
         display.textContent = ('Scissors beat a paper, you win!');
         ++playerScore;
+        checkWinner();
     } else if (computerChoice === 'rock' && playerChoice === 'scissors') {
         display.textContent = ('Rock beats scissors, you lose!');
         ++computerScore;
+        checkWinner();
     } else if (computerChoice === 'scissors' && playerChoice === 'paper') {
         display.textContent = ('Scissors beat the paper, you lose');
         ++computerScore;
+        checkWinner();
     } else if (computerChoice === 'scissors' && playerChoice === 'rock') {
         display.textContent = ('Rock beats scissors, you win!');
-        ++playerScore;  
+        ++playerScore;
+        checkWinner();
     }
 };
 
@@ -37,7 +44,11 @@ const rock = document.querySelector('#rockButton');
 const paper = document.querySelector('#paperButton');
 const scissors = document.querySelector('#scissors');
 const display = document.querySelector('#display');
+const score = document.querySelector('#score');
 
+
+
+score.textContent = (`${playerScore} : ${computerScore}`);
 
 rock.addEventListener('click', () => {
     oneRound("rock");
@@ -49,4 +60,17 @@ scissors.addEventListener('click', () => {
     oneRound("scissors")
 }); 
 
+    function checkWinner() {
+        if (playerScore < 5 && computerScore < 5) {
+            score.textContent = (`${playerScore} : ${computerScore}`);
+        } else if (playerScore = 5) {
+            score.textContent = (`You reach 5 points, so you win!`)
+        } else {
+            score.textContent = (`Computer reach 5 points, so you lose`)
+        }
+    }
+
+
     
+
+
